af5964ab3898c1dbd7d5a0d4c4ac4740c28d3181
DevAnitha <127579679+DevAnitha@users.noreply.github.com>
2025-11-14 03:12:19 +0000

    added person lookup tables

---
+++ MyApi/Entities/Suffix.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Contact.Domain.Entities
+{
+    [Table("suffix", Schema = "person")]
+    public class Suffix
+    {
+        [Key]
+        [Column("suffix_id")]
+        public int SuffixId { get; set; }
+
+        [Column("code")]
+        public string Code { get; set; } = null!;
+
+        [Column("description")]
+        public string? Description { get; set; }
+
+        // --- Created ---
+        [Column("createdby")]
+        public string? CreatedBy { get; set; }
+
+        [Column("createddate")]
+        public DateTime CreatedDate { get; set; }
+
+        [Column("createdip")]
+        public string? CreatedIp { get; set; }
+
+        // --- Updated ---
+        [Column("updatedby")]
+        public string? UpdatedBy { get; set; }
+
+        [Column("updateddate")]
+        public DateTime? UpdatedDate { get; set; }
+
+        [Column("updatedip")]
+        public string? UpdatedIp { get; set; }
+
+        // --- Deleted ---
+        [Column("deletedby")]
+        public string? DeletedBy { get; set; }
+
+        [Column("deleteddate")]
+        public DateTime? DeletedDate { get; set; }
+
+        [Column("deletedip")]
+        public string? DeletedIp { get; set; }
+
+        // --- Archived ---
+        [Column("archivedby")]
+        public string? ArchivedBy { get; set; }
+
+        [Column("archiveddate")]
+        public DateTime? ArchivedDate { get; set; }
+
+        [Column("archivedip")]
+        public string? ArchivedIp { get; set; }
+
+        public ICollection<CoreIdentity> CoreIdentities { get; set; } = new List<CoreIdentity>();
+    }
+}
